2ef999b3485253079f545deb5d23af714143cb17
WolfLee <liyuan0228@gmail.com>
2013-11-04 00:04:57 +0800

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rosewood;
+
+require __DIR__.'/lib/base.php';
+
+class Routing
+{
+    public function start($instance = $this, $callback = null)
+    {
+        return 0;
+    }
+}
