6219f049c0e309859b39272749e33d4a9f56f730
WolfLee <liyuan0228@gmail.com>
2014-04-05 23:38:10 +0800

    Change hard coded db configuration

--- lib/rosewood/database.php
+++ lib/rosewood/database.php
@@ -5,11 +5,12 @@ use RedBean_Facade as R;
 
 class Database
 {
-  function __construct()
+  function __construct($config = [])
   {
-    $this->config = [];
-    R::setup('mysql:host=localhost;dbname=rosewood-test',
-      'root','123456'); //mysql
+    $this->config = $config;
+    if (!empty($this->config)) {
+      R::setup($this->connectionString($this->config['host'], $this->config['db']), $this->config['user'], $this->config['pass']);
+    }
   }
 
   public function __call($function_name, $arguments)
@@ -20,6 +21,11 @@ class Database
   function __destruct()
   {
     R::close();
+  }
+
+  protected function connectionString($host, $database)
+  {
+    return "mysql:host=$host;dbname=$database";
   }
 }
 
